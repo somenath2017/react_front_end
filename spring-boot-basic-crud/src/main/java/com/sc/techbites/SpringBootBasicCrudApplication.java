8566660cbcd08818f5465ff603bf78f41cf77411
Somenath Chatterjee <somnath07in@gmail.com>
2022-12-15 23:11:44 +0530

    Initial Commit

---
+++ spring-boot-basic-crud/src/main/java/com/sc/techbites/SpringBootBasicCrudApplication.java
@@ -0,0 +1,13 @@
+package com.sc.techbites;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBasicCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBasicCrudApplication.class, args);
+	}
+
+}
---
+++ spring-boot-basic-crud/src/main/java/com/sc/techbites/controller/SpringBootBasicCrudController.java
@@ -0,0 +1,8 @@
+package com.sc.techbites.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SpringBootBasicCrudController {
+
+}
